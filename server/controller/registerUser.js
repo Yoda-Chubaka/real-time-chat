af59af595f00fcc678233ac7841e0e9fcf7ef871
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-08-26 12:51:25 +0300

    Updated

--- server/controller/registerUser.js
+++ server/controller/registerUser.js
@@ -1,21 +1,45 @@
-const UserModel = require("../models/UserModel");
+const UserModel = require("../models/UserModel")
+const bcryptjs = require('bcryptjs')
 
-async function registerUser(request, response) {
+async function registerUser(request,response){
     try {
-        const { name, email, password, profile_pic } = request.body;
+        const { name, email, password, profile_pic } = request.body
 
-        const checkEmail = await UserModel.findOne({ email });
+        const checkEmail = await UserModel.findOne({ email }) //{ name,email}  // null
 
-        if (checkEmail) {
+        if(checkEmail){
             return response.status(400).json({
-                message: "User already exists",
-                error: true
+                message : "User already exists",
+                error : true,
             })
         }
+
+        //password into hashpassword
+        const salt = await bcryptjs.genSalt(10)
+        const hashpassword = await bcryptjs.hash(password, salt)
+
+        const payload = {
+            name,
+            email,
+            profile_pic,
+            password : hashpassword
+        }
+
+        const user = new UserModel(payload);
+        const userSave = await user.save();
+
+        return response.status(201).json({
+            message : "User was created successfully",
+            data : userSave,
+            success : true
+        })
+
     } catch (error) {
         return response.status(500).json({
-            message: error.message || error,
-            error: true
+            message : error.message || error,
+            error : true
         })
     }
 }
+
+module.exports = registerUser
