c8aff582e06e2a67ab21e9aba5dc88faaec99e79
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-08-27 14:34:33 +0300

    Update

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,15 @@
-const express = require('express')
-const registerUser = require('../controller/registerUser')
+const express = require("express");
+const registerUser = require("../controller/registerUser");
+const checkEmail = require("../controller/checkEmail");
+const checkPassword = require("../controller/checkPassword");
 
-const router = express.Router()
+const router = express.Router();
 
 //create user api
-router.post('/register',registerUser)
+router.post('/register', registerUser);
+//create user email
+router.post('/email', checkEmail);
+//create user password
+router.post('/password', checkPassword);
 
-module.exports = router
+module.exports = router;
