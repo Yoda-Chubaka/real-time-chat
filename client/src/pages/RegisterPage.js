b0820fedbec322807ce00947085e506fe12d1c92
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-09-03 16:02:07 +0300

    Updated Register Page styles

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { IoClose } from "react-icons/io5";
 
 const RegisterPage = () => {
     const [data, setData] = useState({
@@ -8,6 +9,7 @@ const RegisterPage = () => {
         password: "",
         profile_pic: ""
     })
+    const [uploadPhoto, setUploadPhoto] = useState("");
 
     const handleOnChange = (event) => {
         const { name, value } = event.target;
@@ -20,6 +22,15 @@ const RegisterPage = () => {
             }
         }) 
     }
+
+    const handleUploadPhoto = (event) => {
+        const file = event.target.files[0];
+
+        setUploadPhoto(file);
+    }
+
+    console.log("UploadPhoto", uploadPhoto)
+
     return (
         <div className="mt-5">
             <div className="bg-white w-full max-w-sm mx-2 rounded overflow-hidden p-4">
@@ -71,8 +82,15 @@ const RegisterPage = () => {
                     <div className="flex flex-col gap-1">
                         <label htmlFor="profile_pic">Photo: 
 
-                            <div className="h-14 bg-slate-200 flex justify-center items-center border rounded hover:border-primary">
-                                <p className="text-sm">Upload profile photo</p>
+                            <div className="h-14 bg-slate-200 flex justify-center items-center border rounded hover:border-primary cursor-pointer">
+                                <p className="text-base max-w-[300px] text-ellipsis line-clamp-1">
+                                    {
+                                        uploadPhoto.name? uploadPhoto.name: "Upload profile photo"
+                                    }
+                                </p>
+                                <button className="text-lg ml-2 hover:text-red-600">
+                                    <IoClose/>
+                                </button>
 
                             </div>
 
@@ -87,6 +105,7 @@ const RegisterPage = () => {
                             id="profile_pic"
                             name="profile_pic"
                             className="bg-slate-100 px-2 py-1 focus:outline-primary hidden"
+                            onChange={handleUploadPhoto}
                         />
                     </div>
                 </form>
