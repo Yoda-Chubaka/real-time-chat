2b8070252f278c0794e8416de409e7b9cc52089a
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-09-05 18:32:41 +0300

    Added Email, Password pages, and Avatar Component

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -78,8 +78,9 @@ const RegisterPage = () => {
 
   return (
     <div className='mt-5'>
-        <div className='bg-white w-full max-w-md  rounded overflow-hidden p-4 mx-auto'>
-          <h3>Welcome to Chat app!</h3>
+      <div className='bg-white w-full max-w-md  rounded overflow-hidden p-4 mx-auto'>
+
+        <h3>Welcome to Chat app!</h3>
 
           <form className='grid gap-4 mt-5' onSubmit={handleSubmit}>
               <div className='flex flex-col gap-1'>
@@ -163,7 +164,7 @@ const RegisterPage = () => {
 
           </form>
 
-          <p className='my-3 text-center'>Already have account ? <Link to={"/email"} className='hover:text-primary font-semibold'>Login</Link></p>
+          <p className='my-3 text-center'>Already have account? <Link to={"/email"} className='hover:text-primary font-semibold'>Login</Link></p>
         </div>
     </div>
   )
@@ -175,101 +176,5 @@ export default RegisterPage
 
 
 
-
-
-// return (
-//         <div className="mt-5">
-//             <div className="bg-white w-full max-w-sm rounded overflow-hidden p-4 mx-auto">
-//                 <h3>Welcome to Real Time Chat!</h3>
-
-//                 <form className="grid gap-4 mt-5" onSubmit={handleSubmit}>
-//                     <div className="flex flex-col gap-1">
-//                         <label htmlFor="name">Name: </label>
-//                         <input
-//                             type="text"
-//                             id="name"
-//                             name="name"
-//                             placeholder="Enter your name"
-//                             className="bg-slate-100 px-2 py-1 focus:outline-primary"
-//                             value={data.name}
-//                             onChange={handleOnChange}
-//                             required
-//                         />
-//                     </div>
-
-//                     <div className="flex flex-col gap-1">
-//                         <label htmlFor="email">Email: </label>
-//                         <input
-//                             type="email"
-//                             id="email"
-//                             name="email"
-//                             placeholder="Enter your email"
-//                             className="bg-slate-100 px-2 py-1 focus:outline-primary"
-//                             value={data.email}
-//                             onChange={handleOnChange}
-//                             required
-//                         />
-//                     </div>
-
-//                     <div className="flex flex-col gap-1">
-//                         <label htmlFor="password">Password: </label>
-//                         <input
-//                             type="password"
-//                             id="password"
-//                             name="password"
-//                             placeholder="Enter your password"
-//                             className="bg-slate-100 px-2 py-1 focus:outline-primary"
-//                             value={data.password}
-//                             onChange={handleOnChange}
-//                             required
-//                         />
-//                     </div>
-
-//                     <div className="flex flex-col gap-1">
-//                         <label htmlFor="profile_pic">Photo: 
-
-//                             <div className="h-14 bg-slate-200 flex justify-center items-center border rounded hover:border-primary cursor-pointer">
-//                                 <p className="text-base max-w-[300px] text-ellipsis line-clamp-1">
-//                                     {
-//                                         uploadPhoto?.name? uploadPhoto?.name: "Upload profile photo"
-//                                     }
-//                                 </p>
-//                                 {
-//                                     uploadPhoto?.name && (
-//                                         <button className="text-lg ml-2 hover:text-red-600" onClick={handleClearUploadPhoto}>
-//                                             <IoClose/>
-//                                         </button>
-//                                     )
-//                                 }
-                                
-
-//                             </div>
-
-//                             {/* <button type="button" className="h-14 bg-slate-200 flex justify-center items-center border rounded hover:border-primary w-full text-sm">
-//                                 Upload profile photo
-//                             </button> */}
-
-//                         </label>
-
-//                         <input
-//                             type="file"
-//                             id="profile_pic"
-//                             name="profile_pic"
-//                             className="bg-slate-100 px-2 py-1 focus:outline-primary hidden"
-//                             onChange={handleUploadPhoto}
-//                         />
-//                     </div>
-
-//                     <button className="bg-primary text-lg px-4 py-1 hover:bg-secondary rounded mt-2 font-bold text-white leading-relaxed tracking-wide">
-//                         Register
-//                     </button>
-//                 </form>
-
-//                 <p className="my-3 text-center">
-//                     Already have account ? <Link to={"/email"} className="hover:text-primary font-semibold">Login</Link>
-//                 </p>
-//             </div>
-//         </div>
-//     )
 
 
