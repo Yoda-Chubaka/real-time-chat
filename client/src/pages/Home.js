15a3daaa742aefa3d98139a484b4426adefab95b
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-09-06 14:20:46 +0300

    Added Redux

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Outlet } from 'react-router-dom';
+import axios from "axios";
 
 const Home = () => {
+
+  const fetchUserDetails = async () => {
+    try {
+      const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`;
+      const response = await axios({
+        url: URL,
+        withCredentials: true
+      })
+
+      console.log("current user details", response)
+    } catch (error) {
+        console.log("error", error)
+    }
+  }
+
+  useEffect(() => {
+    fetchUserDetails()
+  }, [])
+
   return (
     <div>
           Home
