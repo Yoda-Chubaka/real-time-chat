3218774889f5c58b458bcc4eefcc63d0f682ad56
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-09-25 10:43:14 +0300

    Added sockets.io

---
+++ server/socket/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { Server } = require("socket.io");
+const http = require("http");
+
+const app = express();
+
+// socket connection
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin : process.env.FRONTEND_URL,
+        credentials : true
+    }
+})
+
+
+io.on("connection", (socket) => {
+    console.log("connect user:", socket.id);
+
+    // disconnect
+    io.on("disconnect", () => {
+        console.log("disconnect user:", socket.id);
+    })
+})
+
+module.exports= {
+    app,
+    server
+}
