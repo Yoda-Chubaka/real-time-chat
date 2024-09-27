c177df59ab7ed698eecc5dad892fc58600cab1e6
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-09-27 11:00:14 +0300

    Created Message page

--- server/socket/index.js
+++ server/socket/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { Server } = require("socket.io");
 const http = require("http");
 const getUserDetailsFromToken = require("../helpers/getUserDetailsFromToken");
+const UserModel = require('../models/UserModel');
 
 const app = express();
 
@@ -29,9 +30,23 @@ io.on("connection", async (socket) => {
 
     // create a room
     socket.join(user?._id);
-    onlineUser.add(user?._id);
+    onlineUser.add(user?._id?.toString());
 
     io.emit("onlineUser", Array.from(onlineUser));
+
+    socket.on("message-page", async (userId) => {
+        console.log("userId", userId);
+        const userDetails = await UserModel.findById(userId).select("-password");
+
+        const payload = {
+            _id: userDetails?._id,
+            name: userDetails?.name,
+            email: userDetails?.email,
+            profile_pic: userDetails?.profile_pic,
+            online: onlineUser.has(userId)
+        }
+        socket.emit("message-user", payload);
+    })
 
     console.log("user:", user)
 
