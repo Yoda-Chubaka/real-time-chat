64bcca65dfd42e182213beb609df945ff49ad06f
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-12-02 16:55:21 +0200

    Updated sockets for conversations

--- server/models/ConversationModel.js
+++ server/models/ConversationModel.js
@@ -1,49 +1,54 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const messageSchema = new mongoose.Schema({
-    text: {
-        type: String,
-        default: ""
+    text : {
+        type : String,
+        default : ""
     },
-    imageUrl: {
-        type: String,
-        default: ""
+    imageUrl : {
+        type : String,
+        default : ""
     },
-    videoUrl: {
-        type: String,
-        default: ""
+    videoUrl : {
+        type : String,
+        default : ""
     },
-    seen: {
-        type: Boolean,
-        default: false
+    seen : {
+        type : Boolean,
+        default : false
     },
-}, {
-    timestamps: true
+    messageByUserId : {
+        type : mongoose.Schema.ObjectId,
+        required : true,
+        ref : 'User'
+    }
+},{
+    timestamps : true
 })
 
 const conversationSchema = new mongoose.Schema({
-    sender: {
-        type: mongoose.Schema.ObjectId,
-        required: true,
-        ref: "User"
+    sender : {
+        type : mongoose.Schema.ObjectId,
+        required : true,
+        ref : 'User'
     },
-    receiver: {
-        type: mongoose.Schema.ObjectId,
-        required: true,
-        ref: "User"
+    receiver : {
+        type : mongoose.Schema.ObjectId,
+        required : true,
+        ref : 'User'
     },
-    messages: [
+    messages : [
         {
-        type: mongoose.Schema.ObjectId,
-        ref: "Message"
-    }
+            type : mongoose.Schema.ObjectId,
+            ref : 'Message'
+        }
     ]
-}, {
-    timestamps: true
+},{
+    timestamps : true
 })
 
-const MessageModel = mongoose.model("Message", messageSchema);
-const ConversationModel = mongoose.model("Conversation", conversationSchema);
+const MessageModel = mongoose.model('Message',messageSchema)
+const ConversationModel = mongoose.model('Conversation',conversationSchema)
 
 module.exports = {
     MessageModel,
--- server/socket/index.js
+++ server/socket/index.js
@@ -3,6 +3,8 @@ const { Server } = require("socket.io");
 const http = require("http");
 const getUserDetailsFromToken = require("../helpers/getUserDetailsFromToken");
 const UserModel = require('../models/UserModel');
+const { ConversationModel, MessageModel } = require("../models/ConversationModel");
+// const getConversation = require('../helpers/getConversation')
 
 const app = express();
 
@@ -48,9 +50,51 @@ io.on("connection", async (socket) => {
         socket.emit("message-user", payload);
     })
 
-    // new message
-    socket.on("new message", (data) => {
-        console.log("new message", data)
+     //new message
+     socket.on('new message',async(data)=>{
+
+        //check conversation is available both user
+
+        let conversation = await ConversationModel.findOne({
+            "$or" : [
+                { sender : data?.sender, receiver : data?.receiver },
+                { sender : data?.receiver, receiver :  data?.sender}
+            ]
+        })
+
+        //if conversation is not available
+        if(!conversation) {
+            const createConversation = await ConversationModel({
+                sender : data?.sender,
+                receiver : data?.receiver
+            })
+            conversation = await createConversation.save()
+        }
+
+        const message = new MessageModel({
+            text : data.text,
+            imageUrl : data.imageUrl,
+            videoUrl : data.videoUrl,
+            messageByUserId :  data?.messageByUserId,
+          })
+          const saveMessage = await message.save()
+  
+          const updateConversation = await ConversationModel.updateOne({ _id : conversation?._id },{
+              "$push" : { messages : saveMessage?._id }
+          })
+  
+          const getConversationMessage = await ConversationModel.findOne({
+              "$or" : [
+                  { sender : data?.sender, receiver : data?.receiver },
+                  { sender : data?.receiver, receiver :  data?.sender}
+              ]
+          }).populate('messages').sort({ updatedAt : -1 })
+  
+  
+          io.to(data?.sender).emit('message',getConversationMessage?.messages || [])
+          io.to(data?.receiver).emit('message',getConversationMessage?.messages || [])
+
+        
     })
 
     // disconnect
