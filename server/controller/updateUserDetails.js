4afdb79852cf0813bf4bf126a136b5ecfbd4f725
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-12-10 13:08:27 +0200

    Bug fixing

--- server/controller/updateUserDetails.js
+++ server/controller/updateUserDetails.js
@@ -1,33 +1,34 @@
-const getUserDetailsFromToken = require("../helpers/getUserDetailsFromToken");
-const UserModel = require("../models/UserModel");
+const getUserDetailsFromToken = require("../helpers/getUserDetailsFromToken")
+const UserModel = require("../models/UserModel")
 
-async function updateUserDetails(request, response) {
+async function updateUserDetails(request,response){
     try {
-        const token = request.cookies.token || "";
+        const token = request.cookies.token || ""
 
-        const user = await getUserDetailsFromToken(token);
+        const user = await getUserDetailsFromToken(token)
 
-        const {name, profile_pic} = request.body;
+        const { name, profile_pic } = request.body
 
-        const updateUser = await UserModel.updateOne({ _id: user._id }, {
+        const updateUser = await UserModel.updateOne({ _id : user._id },{
             name,
             profile_pic
-        });
-
-        const userInformation = await UserModel.findById(user._id);
-
-        return response.status(200).json({
-            message: "User updated successfully",
-            data: userInformation,
-            success: true
         })
+
+        const userInfomation = await UserModel.findById(user._id)
+
+        return response.json({
+            message : "user update successfully",
+            data : userInfomation,
+            success : true
+        })
+
 
     } catch (error) {
         return response.status(500).json({
-            message: error.message || error,
-            error: true
+            message : error.message || error,
+            error : true
         })
     }
 }
 
-module.exports = updateUserDetails;
+module.exports = updateUserDetails
